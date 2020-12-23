4032032182d51f7c5cd291771e60a60f8a7dc884
Leatoki <jingl1224@126.com>
2020-12-23 22:44:54 +0800

    驱动初始化类

--- src/main/java/org/leatoki/uiautotest/init/DriverInit.java
+++ src/main/java/org/leatoki/uiautotest/init/DriverInit.java
@@ -1,7 +1,5 @@
 package org.leatoki.uiautotest.init;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,6 +8,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 
+/**
+ * 驱动初始化类
+ */
 public class DriverInit {
     public static WebDriver driver;
 
@@ -17,22 +18,21 @@ public class DriverInit {
      * 谷歌浏览器驱动
      *
      * @return driver
+     * @throws IOException 异常
      */
     public static WebDriver chromeDriverinit() throws IOException {
-        Logger logger = LogManager.getLogger();
-
-        logger.info("配置浏览器驱动文件路径 Start");
+        LoggerInit.loggerInit().info("配置浏览器驱动文件路径 Start");
         System.setProperty("webdriver.chrome.driver", "src/main/resources/driver/chromedriver.exe");
-        logger.info("配置浏览器驱动文件路径 End");
+        LoggerInit.loggerInit().info("配置浏览器驱动文件路径 End");
 
-        logger.info("========== 根据驱动是否加载配置参数新建驱动 Start ==========");
+        LoggerInit.loggerInit().info("========== 根据驱动是否加载配置参数新建驱动 Start ==========");
 
         // 获取驱动配置
         if ("1".equals(ConfigInit.getProperty("driveroption"))) {
             ChromeOptions options = new ChromeOptions();
             HashMap<String, Object> config = new HashMap<>();
 
-            logger.info("初始化驱动配置选项 Start");
+            LoggerInit.loggerInit().info("初始化驱动配置选项 Start");
             // 设置禁止图片
             config.put("profile.managed_default_content_settings.images", 2);
             // 设置禁止cookies
@@ -44,24 +44,24 @@ public class DriverInit {
             // 加载指定插件
             File file = new File("src/main/resources/crx/selenium-ide.crx");
             options.addExtensions(file);
-            logger.info("初始化驱动配置选项 End");
+            LoggerInit.loggerInit().info("初始化驱动配置选项 End");
 
-            logger.info("添加驱动参数 Start");
+            LoggerInit.loggerInit().info("添加驱动参数 Start");
             options.setExperimentalOption("prefs", config);
-            logger.info("添加驱动参数 End");
+            LoggerInit.loggerInit().info("添加驱动参数 End");
 
-            logger.info("根据配置新建驱动 Start");
+            LoggerInit.loggerInit().info("根据配置新建驱动 Start");
             driver = new ChromeDriver(options);
-            logger.info("根据配置新建驱动 End");
+            LoggerInit.loggerInit().info("根据配置新建驱动 End");
         } else if ("0".equals(ConfigInit.getProperty("driveroption"))) {
-            logger.info("无配置新建驱动 Start");
+            LoggerInit.loggerInit().info("无配置新建驱动 Start");
             driver = new ChromeDriver();
-            logger.info("无配置新建驱动 End");
+            LoggerInit.loggerInit().info("无配置新建驱动 End");
         } else {
-            logger.error("驱动是否加载配置参数值不支持，请验证！");
+            LoggerInit.loggerInit().error("驱动是否加载配置参数值不支持，请验证！");
         }
 
-        logger.info("========== 根据驱动是否加载配置参数新建驱动 End ==========");
+        LoggerInit.loggerInit().info("========== 根据驱动是否加载配置参数新建驱动 End ==========");
 
         return driver;
     }
