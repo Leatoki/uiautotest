c745f73e84b918ee03595810a71add437a33bcb3
Leatoki <jingl1224@126.com>
2020-12-23 22:41:08 +0800

    鼠标键盘操作初始化类

--- src/main/java/org/leatoki/uiautotest/init/ActionInit.java
+++ src/main/java/org/leatoki/uiautotest/init/ActionInit.java
@@ -2,11 +2,14 @@ package org.leatoki.uiautotest.init;
 
 import org.openqa.selenium.interactions.Actions;
 
+/**
+ * 鼠标键盘操作初始化类
+ */
 public class ActionInit {
     /**
-     * Action初始化
+     * Actions初始化
      *
-     * @return Action
+     * @return Actions
      */
     public static Actions actionInit() {
         return new Actions(DriverInit.driver);
