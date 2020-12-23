ac25e8f67f7277d323627fbbdaa956be5be3eaeb
Leatoki <jingl1224@126.com>
2020-12-23 22:50:35 +0800

    显性等待初始化类

--- src/main/java/org/leatoki/uiautotest/init/WaitInit.java
+++ src/main/java/org/leatoki/uiautotest/init/WaitInit.java
@@ -1,17 +1,15 @@
 package org.leatoki.uiautotest.init;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.IOException;
 
+/**
+ * 显性等待初始化类
+ */
 public class WaitInit {
     private static WebDriverWait wait;
 
-    /*
-     * 初始化 wait 实例
-     */
     static {
         String second;
 
@@ -30,14 +28,12 @@ public class WaitInit {
      * @return wait
      */
     public static WebDriverWait getWait() throws IOException {
-        Logger logger = LogManager.getLogger();
-
         if (wait == null) {
-            logger.info("根据配置信息新建显性等待 Start");
+            LoggerInit.loggerInit().info("根据配置信息新建显性等待 Start");
             String second = ConfigInit.getProperty("elementWaitSecond");
             long sec = Long.parseLong(second);
             wait = new WebDriverWait(DriverInit.driver, sec == 0 ? 30000 : sec);
-            logger.info("根据配置信息新建显性等待 End");
+            LoggerInit.loggerInit().info("根据配置信息新建显性等待 End");
         }
 
         return wait;
