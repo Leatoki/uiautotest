e79001c0d03b63543376538919beb5f239c5dc54
Leatoki <jingl1224@126.com>
2021-01-12 22:01:16 +0800

    页面操作类

--- src/main/java/org/leatoki/uiautotest/operation/PageOperation.java
+++ src/main/java/org/leatoki/uiautotest/operation/PageOperation.java
@@ -1,55 +1,57 @@
 package org.leatoki.uiautotest.operation;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.leatoki.uiautotest.init.LoggerInit;
 import org.openqa.selenium.WebDriver;
 import org.leatoki.uiautotest.init.ConfigInit;
 import org.leatoki.uiautotest.init.DriverInit;
 
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * 页面操作类
+ */
 public class PageOperation {
     /**
      * 启动浏览器并打开网址
      *
-     * @param url 测试地址
+     * @param url    测试地址
+     * @param remark 备注
+     * @throws Exception 异常
      */
-    public static void openUrl(String url) throws IOException {
-        Logger logger = LogManager.getLogger();
-        WebDriver dr = DriverInit.chromeDriverinit();
+    public static void openUrl(String url, String remark) throws Exception {
+        WebDriver dr = DriverInit.chromeDriverInit();
 
-        logger.info("========== 开启谷歌浏览器并打开指定地址：" + url + " Start ==========");
+        LoggerInit.loggerInit().info("========== 开启谷歌浏览器并打开指定地址：" + url + " Start ==========");
 
-        logger.info("设置页面最大化 Start");
+        LoggerInit.loggerInit().info("设置页面最大化 Start");
         dr.manage().window().maximize();
-        logger.info("设置页面最大化 End");
+        LoggerInit.loggerInit().info("设置页面最大化 End");
 
-        if ("1".equals(ConfigInit.getProperty("isdeimplicitlywait"))) {
-            logger.info("设置全局隐性等待时长 Start");
-            String secondwait = ConfigInit.getProperty("implicitlywaitsecond");
+        if ("1".equals(ConfigInit.getProperty("isDeImplicitlyWait"))) {
+            LoggerInit.loggerInit().info("设置全局隐性等待时长 Start");
+            String secondwait = ConfigInit.getProperty("implicitlyWaitSecond");
             if (secondwait != null && secondwait.length() != 0) {
                 long secwait = Long.parseLong(secondwait);
                 dr.manage().timeouts().implicitlyWait(secwait, TimeUnit.SECONDS);
             }
-            logger.info("设置全局隐性等待时长 End");
+            LoggerInit.loggerInit().info("设置全局隐性等待时长 End");
         }
 
-        logger.info("打开页面 Start");
+        LoggerInit.loggerInit().info(remark + " Start");
         dr.get(url);
-        logger.info("打开页面 End");
+        LoggerInit.loggerInit().info(remark + " End");
 
-        logger.info("========== 开启谷歌浏览器并打开指定地址：" + url + " End ==========");
+        LoggerInit.loggerInit().info("========== 开启谷歌浏览器并打开指定地址：" + url + " End ==========");
     }
 
     /**
      * 关闭浏览器
+     *
+     * @param remark 备注
      */
-    public static void closeBrowse() {
-        Logger logger = LogManager.getLogger();
-
-        logger.info("========== 关闭浏览器 Start ==========");
+    public static void closeBrowse(String remark) {
+        LoggerInit.loggerInit().info(remark + " Start");
         DriverInit.driver.quit();
-        logger.info("========== 关闭浏览器 End ==========");
+        LoggerInit.loggerInit().info(remark + " End");
     }
 }
